154b643ef644f86b7f644102953ed347284453b7
Sarthak-ai0 <sarthaksharmaa2005@gmail.com>
2025-12-29 16:45:24 +0530

    age calculator

---
+++ Python Basics/age_calculator.py
@@ -0,0 +1,5 @@
+print("welcome to the age calculator".upper())
+birth_year =int(input("enter your birth year: ".capitalize()))
+present = 2025
+age = present - birth_year
+print("you are {} years old".format(age).capitalize())
